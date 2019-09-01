1383cd3b53f9c62b23f4ca1d52d9f19b8896f5a2
tomoro <n.tomoro13@gmail.com>
2019-09-01 23:32:36 +0900

    Updates

---
+++ precache-manifest.490fef01ec5a1e93f0d113648c01913e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d807c9af5c9f119015322bb3b712aca",
+    "url": "/ui-dojo/index.html"
+  },
+  {
+    "revision": "0a2410583a6a5b7853fa",
+    "url": "/ui-dojo/static/css/main.17bd757c.chunk.css"
+  },
+  {
+    "revision": "19a3bc7f6fe431a36939",
+    "url": "/ui-dojo/static/js/2.bd6896d3.chunk.js"
+  },
+  {
+    "revision": "0a2410583a6a5b7853fa",
+    "url": "/ui-dojo/static/js/main.1817faf1.chunk.js"
+  },
+  {
+    "revision": "0cb7165cbcc3cf103994",
+    "url": "/ui-dojo/static/js/runtime~main.c66e70cc.js"
+  },
+  {
+    "revision": "521b16f789962e53790d31962273a4d4",
+    "url": "/ui-dojo/static/media/slider01.521b16f7.jpg"
+  },
+  {
+    "revision": "c97a47a15919914ce37d4ee1100ad7ea",
+    "url": "/ui-dojo/static/media/slider02.c97a47a1.jpg"
+  },
+  {
+    "revision": "21341982a14865013f38d9a7467f7046",
+    "url": "/ui-dojo/static/media/slider03.21341982.jpg"
+  }
+]);
